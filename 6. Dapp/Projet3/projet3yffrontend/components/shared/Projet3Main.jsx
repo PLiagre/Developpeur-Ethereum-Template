476d592aef42b3309ea2b800f9363d8f7f067a7b
flo <f.imbert4@gmail.com>
2024-06-19 17:47:20 +0200

    {children} removed from Projet3Main.jsx

--- 6. Dapp/Projet3/projet3yffrontend/components/shared/Projet3Main.jsx
+++ 6. Dapp/Projet3/projet3yffrontend/components/shared/Projet3Main.jsx
@@ -4,49 +4,49 @@ import { useToast } from "../ui/use-toast";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 
-const Projet3Main = ({children}) => {
-    return ( 
+const Projet3Main = () => {
+    return (
         <>
-        <p>Accueil</p>
-        <div className="flex">
-             <Input placeholder="Votre adresse" onChange={(e) => setNumber(e.target.value)} />
-             <Button variant="outline">
-             Ajout Adresse</Button> 
-        </div>
-        <div className="flex">
-            <Button variant="outline">
-             Commencer l'enregistrement des propositions</Button> 
-        </div>
-        <div className="flex">
-             <Input placeholder="Votre proposition" onChange={(e) => setNumber(e.target.value)} />
-             <Button variant="outline">
-            Enregistrer une proposition</Button> 
-        </div>
-        <div>
-            <Button variant="outline">
-             Mettre fin à l'enregistrement des propositions</Button> 
-        </div>
-        <div className="flex">
-            <Button variant="outline">
-             Commencer la session de vote</Button> 
-        </div>
-        <div className="flex">
-             <Input placeholder="Id de la proposition" onChange={(e) => setNumber(e.target.value)} />
-             <Button variant="outline">
-            Voter</Button> 
-        </div>
-        <div>
-            <Button variant="outline">
-             Mettre fin à la session de vote</Button> 
-        </div>
-        <div>
-            <Button variant="outline">
-             Compter les votes</Button> 
-        </div>
-        <div>
-            <Button variant="outline">
-             Afficher le résultat</Button> 
-        </div>
+            <p>Accueil</p>
+            <div className="flex">
+                <Input placeholder="Votre adresse" onChange={(e) => setNumber(e.target.value)} />
+                <Button variant="outline">
+                    Ajout Adresse</Button>
+            </div>
+            <div className="flex">
+                <Button variant="outline">
+                    Commencer l'enregistrement des propositions</Button>
+            </div>
+            <div className="flex">
+                <Input placeholder="Votre proposition" onChange={(e) => setNumber(e.target.value)} />
+                <Button variant="outline">
+                    Enregistrer une proposition</Button>
+            </div>
+            <div>
+                <Button variant="outline">
+                    Mettre fin à l'enregistrement des propositions</Button>
+            </div>
+            <div className="flex">
+                <Button variant="outline">
+                    Commencer la session de vote</Button>
+            </div>
+            <div className="flex">
+                <Input placeholder="Id de la proposition" onChange={(e) => setNumber(e.target.value)} />
+                <Button variant="outline">
+                    Voter</Button>
+            </div>
+            <div>
+                <Button variant="outline">
+                    Mettre fin à la session de vote</Button>
+            </div>
+            <div>
+                <Button variant="outline">
+                    Compter les votes</Button>
+            </div>
+            <div>
+                <Button variant="outline">
+                    Afficher le résultat</Button>
+            </div>
         </>
     )
 }
