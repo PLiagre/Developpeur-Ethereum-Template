d7729fd6c79ee927f8a0e360d95a40a8328ba1b4
flo <f.imbert4@gmail.com>
2024-06-20 10:21:15 +0200

    dependencies added in backend =>     "@nomicfoundation/hardhat-toolbox": "^5.0.0",     "@openzeppelin/contracts": "^5.0.2"
    file modified in backend =>
    Voting.sol projet1 to Voting.sol projet2 ( code cleaner than the first one and comments added )
    
    dependencies added in frontend =>         "dotenv": "^16.4.5",
    
    folder added in frontend =>
    constants/utils.js

--- 6. Dapp/Projet3/projet3yfbackend/ignition/modules/Voting.js
+++ 6. Dapp/Projet3/projet3yfbackend/ignition/modules/Voting.js
@@ -1,8 +1,5 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
-const JAN_1ST_2030 = 1893456000;
-const ONE_GWEI = 1_000_000_000n;
-
 module.exports = buildModule("VotingModule", (m) => {
 
   const voting = m.contract("Voting");
