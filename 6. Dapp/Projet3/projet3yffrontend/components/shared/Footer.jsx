170e6dfb88b8b6b41d63993c3496c4467cbb6249
ydaci <ydaci59@gmail.com>
2024-06-23 22:16:24 +0200

    Css + bouton dark/light mode

--- 6. Dapp/Projet3/projet3yffrontend/components/shared/Footer.jsx
+++ 6. Dapp/Projet3/projet3yffrontend/components/shared/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = ({children}) => {
     return ( 
-    <footer className="footer">
+    <footer className="footer text-2xl font-bold text-gray-800 dark:text-white">
         Tous droits réservés &copy; Yanis et Florian {new Date().getFullYear()}    
     </footer>
     )
--- 6. Dapp/Projet3/projet3yffrontend/components/shared/Projet3Main.jsx
+++ 6. Dapp/Projet3/projet3yffrontend/components/shared/Projet3Main.jsx
@@ -3,6 +3,7 @@ import AddProposal from "./AddProposal";
 import Vote from "./Vote";
 import TallyVote from "./TallyVote";
 import GetWinningProposal from "./GetWinningProposal"
+import { Button } from "../ui/button";
 // import Events from "./Events"
 
 import { useState, useEffect } from "react"
@@ -15,8 +16,36 @@ import WorkflowStatus from "./WorkflowStatus";
 
 const Projet3Main = () => {
 
+    const [darkMode, setDarkMode] = useState(false);
+
+    const toggleDarkMode = () => {
+        setDarkMode(!darkMode);
+        if (!darkMode) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        }
+    };
+
+    useEffect(() => {
+        const storedDarkMode = localStorage.getItem('darkMode') === 'true';
+        setDarkMode(storedDarkMode);
+        if (storedDarkMode) {
+            document.documentElement.classList.add('dark');
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('darkMode', darkMode);
+    }, [darkMode]);
+
+    
+
     return (
-        <div className="space-y-5">
+        <div className={`space-y-5 ${darkMode ? 'dark' : ''}`}>
+            <Button onClick={toggleDarkMode} className="p-2 border rounded">
+                Dark or Light Mode ?
+            </Button>
             <WorkflowStatus />
             <AddVoter />
             <AddProposal />
