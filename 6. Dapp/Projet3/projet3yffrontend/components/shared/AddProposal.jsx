ad8deb971e94252367e0b72c6ea4da4180220c5e
ydaci <ydaci59@gmail.com>
2024-06-24 17:49:36 +0200

    Ajout de la visibilité des proposals - correctif ok + css

--- 6. Dapp/Projet3/projet3yffrontend/components/shared/AddProposal.jsx
+++ 6. Dapp/Projet3/projet3yffrontend/components/shared/AddProposal.jsx
@@ -1,7 +1,7 @@
 'use client';
 // config
 import { useState, useEffect } from "react";
-import { useAccount, useBalance, useReadContract, useWriteContract, useWaitForTransactionReceipt, useSendTransaction } from "wagmi";
+import { useAccount, useWriteContract, useWaitForTransactionReceipt } from "wagmi";
 import { contractAddress, contractAbi } from "@/constants";
 import Informations from "./Information";
 
@@ -9,43 +9,18 @@ import Informations from "./Information";
 import { useToast } from "../ui/use-toast";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
-import { Alert, AlertDescription, AlertTitle } from "../ui/alert";
 
-
-const AddProposal = ({ getEvents }) => {
-  const { toast } = useToast()
+const AddProposal = () => {
+  const { toast } = useToast();
   const { address } = useAccount();
-  const [proposal, setProposal] = useState("")
+  const [proposal, setProposal] = useState("");
   const [proposals, setProposals] = useState([]);
   const [proposalCount, setProposalCount] = useState(0);
+  const [pendingProposal, setPendingProposal] = useState(null);
 
   const { data: hash, isPending, error, writeContract } = useWriteContract();
   const { isLoading: isConfirming, isSuccess: isConfirmed, refetch } =
-    useWaitForTransactionReceipt({
-      hash,
-    })
-    
-    const { data: countData } = useReadContract({
-      address: contractAddress,
-      abi: contractAbi,
-      functionName: 'getOneProposal',
-      args: [proposalCount], // Utilisation du compteur local comme index
-    });
-  
-    const fetchProposals = async () => {
-      let proposalsArray = [];
-      console.log("fetch 1" + proposalCount);
-      for (let i = 0; i < proposalCount; i++) {
-        try {
-          const proposal = await readContract('getOneProposal', [i]);
-          console.log(`Fetched proposal with id ${i}:`, proposal);
-          proposalsArray.push(proposal);
-        } catch (error) {
-          console.error(`Error fetching proposal with id ${i}:`, error);
-        }
-      }
-      setProposals(proposalsArray);
-    };
+    useWaitForTransactionReceipt({ hash });
 
   const addProposal = async () => {
     if (proposal === "") {
@@ -62,8 +37,8 @@ const AddProposal = ({ getEvents }) => {
           functionName: 'addProposal',
           args: [proposal],
         });
+        setPendingProposal(proposal);
         setProposal('');
-        setProposalCount(prevCount => prevCount + 1); // Incrémente le compteur local
       } catch (error) {
         console.error(error);
       }
@@ -71,40 +46,41 @@ const AddProposal = ({ getEvents }) => {
   };
 
   useEffect(() => {
-    if (isConfirmed) {
-      setProposal('');
-      fetchProposals();
+    if (isConfirmed && pendingProposal) {
+      setProposals([...proposals, { description: pendingProposal }]);
+      setProposalCount(prevCount => prevCount + 1);
+      setPendingProposal(null);
       refetch();
     }
-  }, [isConfirmed, refetch])
-
-
-
+  }, [isConfirmed, pendingProposal, proposals, refetch]);
 
   return (
-    <section className=" space-y-2">
+    <section className="space-y-2">
       <h2 className="font-bold">Add Proposal</h2>
       <div className="flex space-x-2">
-        <Input type="text" placeholder="Votre proposition" onChange={(e) => setProposal(e.target.value)} maxLength={42} className="max-w-80 border-2" />
-        <Button onClick={addProposal} variant="outline" className="bg-lime-400" >
-          Add Proposal</Button>
+        <Input
+          type="text"
+          placeholder="Votre proposition"
+          value={proposal}
+          onChange={(e) => setProposal(e.target.value)}
+          maxLength={42}
+          className="max-w-80 border-2"
+        />
+        <Button onClick={addProposal} variant="outline" className="bg-lime-400">
+          Add Proposal
+        </Button>
       </div>
       <Informations hash={hash} isConfirming={isConfirming} isConfirmed={isConfirmed} error={error} />
       <div>
-        <h3 className="font-bold">Proposals</h3>
-        <ul>
-        {proposals.map((proposal, index) => {
-          console.log(`Rendering proposal with id ${index}:`, proposal);  // Ajout du console.log ici
-          return <li key={index}>{proposal.description}</li>;
-        })}
-        Prop : 
-        {proposals.map((proposal, index) => (
-            <li key={index}>{proposal.description}</li>
+      <h3 className="font-bold text-xl text-gray-600">Proposals</h3>
+      <ul className="mt-2 space-y-2">
+          {proposals.map((proposal, index) => (
+            <li className="p-4 bg-white rounded-lg shadow-sm border border-gray-200" key={index}>{proposal.description}</li>
           ))}
-      </ul>
+        </ul>
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default AddProposal;
